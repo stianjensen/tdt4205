a35cd497b643dfe4f32aac965343c83f6509ee2b
Stian Jensen <me@stianj.com>
2014-02-28 15:36:29 +0100

    Setup PS5 code framework

---
+++ ps5/code/src/typecheck.c
@@ -0,0 +1,51 @@
+#include "typecheck.h"
+extern int outputStage;
+
+data_type_t td(node_t* root);
+data_type_t te(node_t* root);
+data_type_t tv(node_t* root);
+
+void type_error(node_t* root){
+	fprintf(stderr, "Type error at:\n");
+	if(root != NULL){
+		fprintf(stderr,"%s", root->nodetype.text);
+		if(root->nodetype.index == EXPRESSION){
+			fprintf(stderr," (%s)", root->expression_type.text);
+		}
+		fprintf(stderr,"\n");
+	}
+	fprintf(stderr,"Exiting\n");
+	exit(-1);
+}
+
+int equal_types(data_type_t a, data_type_t b)
+{
+
+}
+
+data_type_t typecheck_default(node_t* root)
+{
+	return td(root);
+}
+
+data_type_t typecheck_expression(node_t* root)
+{
+	data_type_t toReturn;
+
+	if(outputStage == 10)
+		fprintf( stderr, "Type checking expression %s\n", root->expression_type.text);
+
+	toReturn = te(root);
+	
+	//Insert additional checking here
+	
+}
+
+data_type_t typecheck_variable(node_t* root){
+	return tv(root);
+}
+
+data_type_t typecheck_assignment(node_t* root)
+{
+
+}
